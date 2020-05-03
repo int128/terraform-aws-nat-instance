0aac098bb592e6727c096cf240435c12b13927f1
Hidetake Iwata <int128@gmail.com>
2020-05-03 12:48:01 +0900

    Specify type to variables (#14)

--- variables.tf
+++ variables.tf
@@ -1,55 +1,68 @@
 variable "enabled" {
   description = "Enable or not costly resources"
+  type        = bool
   default     = true
 }
+
 variable "name" {
   description = "Name for all the resources as identifier"
+  type        = string
 }
 
 variable "vpc_id" {
   description = "ID of the VPC"
+  type        = string
 }
 
 variable "public_subnet" {
   description = "ID of the public subnet to place the NAT instance"
+  type        = string
 }
 
 variable "private_subnets_cidr_blocks" {
   description = "List of CIDR blocks of the private subnets. The NAT instance accepts connections from this subnets"
+  type        = list
 }
 
 variable "private_route_table_ids" {
   description = "List of ID of the route tables for the private subnets. You can set this to assign the each default route to the NAT instance"
+  type        = list
   default     = []
 }
 
 variable "extra_user_data" {
   description = "Extra script to run in the NAT instance"
+  type        = string
   default     = ""
 }
 
 variable "image_id" {
   description = "AMI of the NAT instance. Default to the latest Amazon Linux 2"
+  type        = string
   default     = ""
 }
 
 variable "instance_types" {
   description = "Candidates of spot instance type for the NAT instance. This is used in the mixed instances policy"
+  type        = list
   default     = ["t3.nano", "t3a.nano"]
 }
 
 variable "use_spot_instance" {
   description = "Whether to use spot or on-demand EC2 instance"
+  type        = bool
   default     = true
 }
 
 variable "key_name" {
   description = "Name of the key pair for the NAT instance. You can set this to assign the key pair to the NAT instance"
+  type        = string
   default     = ""
 }
 
 variable "tags" {
   description = "Tags applied to resources created with this module"
+  type        = map
   default     = {}
 }
 
