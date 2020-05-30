dc547b54ffd925a09705ff56bd3fa86fed2d910d
Hidetake Iwata <int128@gmail.com>
2020-05-30 19:00:48 +0900

    Fix to enable snat after reboot

---
+++ runonce.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+# attach the ENI
+aws ec2 attach-network-interface \
+  --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
+  --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)" \
+  --device-index 1 \
+  --network-interface-id "${eni_id}"
+
+# start SNAT
+systemctl enable snat
+systemctl start snat
