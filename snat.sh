e4a0b3310d32d038a89d1c6a7d43498819a40b3e
Glen Ogilvie <glen.ogilvie@oss.co.nz>
2021-02-17 12:46:32 +1300

    my fix to get nat instance working

--- snat.sh
+++ snat.sh
@@ -5,13 +5,16 @@ while ! ip link show dev eth1; do
   sleep 1
 done
 
+sysctl -q -w net.ipv4.conf.all.rp_filter=0
+sysctl -q -w net.ipv4.conf.eth0.rp_filter=0
+sysctl -q -w net.ipv4.conf.eth1.rp_filter=0
+sysctl -q -w net.ipv4.conf.default.rp_filter=0
+
+
 # enable IP forwarding and NAT
 sysctl -q -w net.ipv4.ip_forward=1
 sysctl -q -w net.ipv4.conf.eth1.send_redirects=0
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
-
-# switch the default route to eth1
-ip route del default dev eth0
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # wait for network connection
 curl --retry 10 http://www.example.com
