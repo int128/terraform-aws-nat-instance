38b7da23fc5dc946885a8e93005f126063d9b4c6
Julian Calaby <julian@cabinetsbycomputer.com>
2022-07-26 11:51:23 +1000

    Disable reverse path protection on all interfaces

--- snat.sh
+++ snat.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 set -x
 
-# wait for eth1
+# Wait for eth1
 while ! ip link show dev eth1; do
   sleep 1
 done
 
-# enable IP forwarding and NAT
+# Enable IP forwarding
 sysctl -q -w net.ipv4.ip_forward=1
+
+# Disable ICMP redirects on eth1
 sysctl -q -w net.ipv4.conf.eth1.send_redirects=0
+
+# Configure NAT
 iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+
+# Disable reverse path protection
+for i in $(find /proc/sys/net/ipv4/conf/ -name rp_filter) ; do
+  echo 0 > $i;
+done
 
 # prevent setting the default route to eth0 after reboot
 rm -f /etc/sysconfig/network-scripts/ifcfg-eth0
