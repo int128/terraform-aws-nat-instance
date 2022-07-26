45f73f9e1d7a1094355b17a71efb87664cbda754
Julian Calaby <julian@cabinetsbycomputer.com>
2022-07-26 11:53:19 +1000

    Switch to using eth0 for output

--- snat.sh
+++ snat.sh
@@ -13,21 +13,12 @@ sysctl -q -w net.ipv4.ip_forward=1
 sysctl -q -w net.ipv4.conf.eth1.send_redirects=0
 
 # Configure NAT
-iptables -t nat -A POSTROUTING -o eth1 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # Disable reverse path protection
 for i in $(find /proc/sys/net/ipv4/conf/ -name rp_filter) ; do
   echo 0 > $i;
 done
 
-# prevent setting the default route to eth0 after reboot
-rm -f /etc/sysconfig/network-scripts/ifcfg-eth0
-
-# switch the default route to eth1
-ip route del default dev eth0
-
 # wait for network connection
 curl --retry 10 http://www.example.com
-
-# reestablish connections
-systemctl restart amazon-ssm-agent.service
