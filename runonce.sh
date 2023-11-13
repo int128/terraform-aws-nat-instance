30e805f2d16b6b988494a382c8a24869e7887a30
Roman Vrublevskiy <roman@clumio.com>
2023-11-12 20:54:21 -0600

    Add retries for attach-network-interface

--- runonce.sh
+++ runonce.sh
@@ -1,11 +1,15 @@
 #!/bin/bash -x
 
 # attach the ENI
-aws ec2 attach-network-interface \
-  --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
-  --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)" \
-  --device-index 1 \
-  --network-interface-id "${eni_id}"
+end_time=$((SECONDS + 180))
+while [ $SECONDS -lt $end_time ] && ! ip link show dev eth1; do
+  aws ec2 attach-network-interface \
+    --region "$(/opt/aws/bin/ec2-metadata -z  | sed 's/placement: \(.*\).$/\1/')" \
+    --instance-id "$(/opt/aws/bin/ec2-metadata -i | cut -d' ' -f2)" \
+    --device-index 1 \
+    --network-interface-id "${eni_id}"
+    sleep 1
+done
 
 # start SNAT
 systemctl enable snat
